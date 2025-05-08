501ec47083e0e9f50d5721f881f762ff27b417d9
Clarissa <clarissalimadp@usp.br>
2025-05-08 16:08:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ for _, row in filtered.iterrows():
 folium.LayerControl(collapsed=False).add_to(m)
 
 legend_html = """
-<div style="position: fixed; bottom: 50px; left: 50px; width: 200px;
+<div style="position: fixed; bottom: 50px; left: 50px; width: 220px;
 background-color: white; border:2px solid grey; z-index:9999; font-size:14px; padding: 10px;">
 <b>Legenda</b><br>
 <span style="color:red;">●</span> Assassinato<br>
